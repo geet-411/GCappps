aa8fba94607f9d72db0f18b3a4871d8c6d1b93b9
geet-411 <geetanjali41196@gmail.com>
2023-01-13 16:08:42 +0530

    Hi

---
+++ src/main/java/com/GCappps/loanFin/app/GCapppsApplication.java
@@ -0,0 +1,13 @@
+package com.GCappps.loanFin.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GCapppsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GCapppsApplication.class, args);
+	}
+
+}
